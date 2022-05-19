fa56efc0e7c3f7930b182e9ac43cf51455c256b3
Alisha (Jungah Sohn) <aliciasohn815@gmail.com>
2022-05-19 14:07:06 +0200

    syling for font and setup for active navbar link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,30 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const router = useRouter();
+  console.log("router", router);
   return (
-    <nav>
-      <Link href={"/"}>
-        <a>Home</a>
-      </Link>
-      <Link href={"/about"}>
-        <a>About</a>
-      </Link>
-    </nav>
+    <>
+      <nav>
+        <Link href={"/"}>
+          <a className={router.pathname === "/" ? "active" : ""}>Home</a>
+        </Link>
+        <Link href={"/about"}>
+          <a className={router.pathname === "/about" ? "active" : ""}>About</a>
+        </Link>
+      </nav>
+      <style jsx>{`
+        a {
+          text-decoration: none;
+          color: #59626E
+          }
+        .active {
+            color: #1C64F2
+        }
+        }>
+      `}</style>
+    </>
   );
 };
 
